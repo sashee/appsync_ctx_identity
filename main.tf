60e826da965e58a34139c6a06202ffb5de22014d
Tamas Sallai <tamas.sallai@advancedweb.hu>
2022-10-10 13:13:48 +0200

    add lambda authorizer

--- main.tf
+++ main.tf
@@ -41,28 +41,6 @@ resource "aws_cognito_user_group" "user" {
 
 # database
 
-resource "aws_dynamodb_table" "users" {
-  name           = "Users-${random_id.id.hex}"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "sub"
-
-  attribute {
-    name = "sub"
-    type = "S"
-  }
-}
-
-resource "aws_dynamodb_table" "documents" {
-  name           = "Documents-${random_id.id.hex}"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
-
-  attribute {
-    name = "id"
-    type = "S"
-  }
-}
-
 resource "aws_cognito_user" "user1" {
   user_pool_id = aws_cognito_user_pool.pool.id
   username     = "user1"
@@ -107,4 +85,98 @@ resource "aws_appsync_graphql_api" "iam" {
 module "iam-appsync-resources" {
   source  = "./modules/appsync-resources"
   api = aws_appsync_graphql_api.iam
+}
+
+# Lambda
+
+resource "aws_appsync_graphql_api" "lambda" {
+  name                = "lambda-test"
+  schema              = file("schema.graphql")
+  authentication_type = "AWS_LAMBDA"
+	lambda_authorizer_config {
+		authorizer_uri = aws_lambda_function.authorizer.arn
+	}
+}
+
+module "lambda-appsync-resources" {
+  source  = "./modules/appsync-resources"
+  api = aws_appsync_graphql_api.lambda
+}
+
+resource "aws_lambda_permission" "appsync_lambda_authorizer" {
+	action        = "lambda:InvokeFunction"
+	function_name = aws_lambda_function.authorizer.function_name
+	principal     = "appsync.amazonaws.com"
+	source_arn    = aws_appsync_graphql_api.lambda.arn
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  output_path = "/tmp/lambda-${random_id.id.hex}.zip"
+  source {
+    content  = <<EOF
+module.exports.handler = async (event) => {
+	return {
+		isAuthorized: true,
+		deniedFields: [],
+		resolverContext: {
+			this: "is",
+			a: "test",
+		},
+		ttlOverride: 0,
+	};
+};
+EOF
+    filename = "index.js"
+  }
+}
+
+resource "aws_lambda_function" "authorizer" {
+  function_name = "authorizer-${random_id.id.hex}"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  handler = "index.handler"
+  runtime = "nodejs16.x"
+  role    = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "authorizer_loggroup" {
+  name              = "/aws/lambda/${aws_lambda_function.authorizer.function_name}"
+  retention_in_days = 14
+}
+
+data "aws_iam_policy_document" "lambda_exec_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "lambda_exec_policy" {
+  role   = aws_iam_role.lambda_exec.id
+  policy = data.aws_iam_policy_document.lambda_exec_policy.json
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+	{
+	  "Action": "sts:AssumeRole",
+	  "Principal": {
+		"Service": "lambda.amazonaws.com"
+	  },
+	  "Effect": "Allow"
+	}
+  ]
+}
+EOF
 }
